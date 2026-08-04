2c74bdba1b4add031e1810f7b266a6ee18610959
maryamjaved416-tech <maryamjaved416@gmail.com>
2026-08-04 15:35:51 +0500

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,14 +1,15 @@
 import requests
 import json
+
 def emotion_detector(text_to_analyze):
-url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-myobj = { "raw_document": { "text": text_to_analyze } }
-response = requests.post(url, json=myobj, headers=headers)
-formatted_response = json.loads(response.text)
-if formatted_response['status']['code'] == 400:
-return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
-emotions = formatted_response['emotionPredictions'][0]['emotion']
-dominant_emotion = max(emotions, key=emotions.get)
-emotions['dominant_emotion'] = dominant_emotion
-return emotions
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": {"text": text_to_analyze}}
+    response = requests.post(url, json=myobj, headers=headers)
+    formatted_response = json.loads(response.text)
+    if formatted_response['status']['code'] == 400:
+        return {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': None}
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+    return response.text
